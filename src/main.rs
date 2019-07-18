155f3ccff0e39bdcb59583133741930dfc792ce6
Olivia <olivia_github@outlook.com>
2019-07-18 21:27:20 +0100

    Add trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// Hash table trait, allows getting and setting.
+// For now this is not a generic HashTable.
+trait HashTable {
+    fn get(&self, key: &String) -> &String;
+    fn set(&mut self, key: String, value: String) -> Result<(), String>;
+}
+
 fn main() {
     println!("Hello, world!");
 }
