4ea674c160df32400fe60128b119f3a7885e9a20
Olivia <olivia_github@outlook.com>
2019-07-18 21:31:21 +0100

    Add skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 // Hash table trait, allows getting and setting.
 // For now this is not a generic HashTable.
 trait HashTable {
-    fn get(&self, key: &String) -> &String;
+    fn get(&self, key: &String) -> Option<&String>;
     fn set(&mut self, key: String, value: String) -> Result<(), String>;
+}
+
+struct LinearProbingHashTable {
+    store: Vec<(String, String)>,
+}
+
+impl LinearProbingHashTable {
+    fn new() -> Self {
+        Self{store: Vec::new()}
+    }
+}
+
+impl HashTable for LinearProbingHashTable {
+
+    fn get(&self, key: &String) -> Option<&String> {
+        None
+    }
+
+    fn set(&mut self, key: String, value: String) -> Result<(), String> {
+        Ok(())
+    }
 }
 
 fn main() {
